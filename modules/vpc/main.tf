fdfaf56c850708f99b8104971aa7f6761f68f319
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-25 14:08:19 +0530

    feat: create private subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,15 @@ resource "aws_subnet" "public_subnet" {
     Name = "public subnet ${count.index + 1}"
   }
 }
+
+
+resource "aws_subnet" "private_subnet" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  availability_zone       = element(var.private_subnet_cidrs, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public subnet ${count.index + 1}"
+  }
+}
