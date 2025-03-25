7f2da0ecaa986d88b50950e9b64f7bc4cdaa5a83
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-25 14:06:58 +0530

    feat: create public subnet

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,13 @@ variable "cidr_block" {
   description = "CIDR address for vpc"
   type        = string
 }
+
+variable "public_subnet_cidrs" {
+  description = "CIDR for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR for private subnet"
+  type        = string
+}
