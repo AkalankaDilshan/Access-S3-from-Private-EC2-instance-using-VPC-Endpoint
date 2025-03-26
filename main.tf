387876a3f909dd0b1a788cc88408f6a4923bd214
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 12:33:05 +0530

    chor: output public ips

--- main.tf
+++ main.tf
@@ -27,24 +27,26 @@ module "secuiry_group_application" {
   source_security_group_id = module.secuiry_group_bastion.sg_id
 }
 
-module "web_instace-bastion" {
+module "web_instace_bastion" {
   source             = "./modules/ec2"
   instance_name      = "Bastion-ec2"
   instance_type      = "t3.micro"
   subnet_id          = module.main_vpc.public_subnet_id
   ec2_security_group = module.secuiry_group_bastion.sg_id
+  allow_public_ip    = true
   ebs_volume_type    = "gp2"
   ebs_volume_size    = 8
   key_pair_name      = "moba-key"
   depends_on         = [module.secuiry_group_application]
 }
 
-module "web_instace-application" {
+module "web_instace_application" {
   source             = "./modules/ec2"
   instance_name      = "Application-ec2"
   instance_type      = "t3.micro"
   subnet_id          = module.main_vpc.private_subnet_id
   ec2_security_group = module.secuiry_group_application.sg_id
+  allow_public_ip    = true
   ebs_volume_type    = "gp2"
   ebs_volume_size    = 8
   key_pair_name      = "moba-key"
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# output "Bastion-ec2-ip" {
+#   value = module.web_instace_bastion
+# }
+
+# output "Application-ec2-ip" {
+#   value = modu
+# }
