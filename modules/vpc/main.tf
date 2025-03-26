61d72a312190cfd2124963e8561b9f8271fba703
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 14:18:22 +0530

    chor : disable nat gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -63,25 +63,25 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-resource "aws_eip" "elastic_IP_address" {
-  domain = "vpc"
-  tags = {
-    Name = "${var.vpc_name}-vpc-nat-gateway-eip"
-  }
-}
+# resource "aws_eip" "elastic_IP_address" {
+#   domain = "vpc"
+#   tags = {
+#     Name = "${var.vpc_name}-vpc-nat-gateway-eip"
+#   }
+# }
 
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.elastic_IP_address.id
-  subnet_id     = aws_subnet.public_subnet.id
-  tags = {
-    Name = "${var.vpc_name}-nat-gateway"
-  }
-}
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
-}
+# resource "aws_nat_gateway" "nat_gateway" {
+#   allocation_id = aws_eip.elastic_IP_address.id
+#   subnet_id     = aws_subnet.public_subnet.id
+#   tags = {
+#     Name = "${var.vpc_name}-nat-gateway"
+#   }
+# }
+# resource "aws_route" "private_route" {
+#   route_table_id         = aws_route_table.private_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+# }
 
 resource "aws_route_table_association" "private_rt_association" {
   subnet_id      = aws_subnet.private_subnet.id
