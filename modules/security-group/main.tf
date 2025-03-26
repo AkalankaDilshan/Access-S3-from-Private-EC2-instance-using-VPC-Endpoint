9adb1777af89169efb4c42865ea75b72343daa5f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 11:34:49 +0530

    feat: complete ec2 sg

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -7,3 +7,42 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  description       = "allow SSH ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_HTTP" {
+  type              = "ingress"
+  description       = "allow HTTP ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_HTTPS" {
+  type              = "ingress"
+  description       = "allow HTTPS ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "public_allow_all_outbound" {
+  type              = "egress"
+  description       = "Allow ALl outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
