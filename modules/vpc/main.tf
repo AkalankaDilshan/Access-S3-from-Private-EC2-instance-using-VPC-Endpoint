ba7d1cd9ea7216e261726e8100d9c292e960c02a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 11:12:25 +0530

    fix: missing id

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -52,7 +52,7 @@ resource "aws_route" "public_route" {
 
 resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt
+  route_table_id = aws_route_table.public_rt.id
 }
 
 // private route table 
