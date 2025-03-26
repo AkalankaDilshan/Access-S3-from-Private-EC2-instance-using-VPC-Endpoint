ecd4bf31ec5fb20d5b8814926e4224355c262769
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 10:40:50 +0530

    fix(vpc): correct invalid CIDR block in private subnet

--- main.tf
+++ main.tf
@@ -10,5 +10,5 @@ module "VPC" {
   cidr_block           = "192.168.0.0/16"
   availability_zones   = "eu-north-1a"
   public_subnet_cidrs  = "192.168.1.0/24"
-  private_subnet_cidrs = "192.168.3.30/24"
+  private_subnet_cidrs = "192.168.3.0/24"
 }
