ee2e80e7487e3ef3651a760874ea6f23b0502227
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-26 11:10:39 +0530

    fix: route table errors 🔢

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -52,7 +52,7 @@ resource "aws_route" "public_route" {
 
 resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route.public_route.id
+  route_table_id = aws_route_table.public_rt
 }
 
 // private route table 
@@ -63,11 +63,11 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = var.cidr_block
-  gateway_id             = "local"
-}
+# resource "aws_route" "private_route" {
+#   route_table_id         = aws_route_table.private_rt.id
+#   destination_cidr_block = var.cidr_block
+#   gateway_id             = "local"
+# }
 
 resource "aws_route_table_association" "private_rt_association" {
   subnet_id      = aws_subnet.private_subnet.id
