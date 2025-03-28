4f2eda80f386dc9467e102276b28a7bb346483a2
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-03-28 13:24:13 +0530

    chor: call only bastion instance

--- main.tf
+++ main.tf
@@ -21,12 +21,12 @@ module "secuiry_group_bastion" {
   vpc_id              = module.main_vpc.vpc_id
 }
 
-module "secuiry_group_application" {
-  source                   = "./modules/security-group-application"
-  security_group_name      = "ec2-app-sg"
-  vpc_id                   = module.main_vpc.vpc_id
-  source_security_group_id = module.secuiry_group_bastion.sg_id
-}
+# module "secuiry_group_application" {
+#   source                   = "./modules/security-group-application"
+#   security_group_name      = "ec2-app-sg"
+#   vpc_id                   = module.main_vpc.vpc_id
+#   source_security_group_id = module.secuiry_group_bastion.sg_id
+# }
 
 module "web_instace_bastion" {
   source             = "./modules/ec2"
@@ -41,15 +41,15 @@ module "web_instace_bastion" {
   depends_on         = [module.secuiry_group_application]
 }
 
-module "web_instace_application" {
-  source             = "./modules/ec2"
-  instance_name      = "Application-ec2"
-  instance_type      = "t3.micro"
-  subnet_id          = module.main_vpc.private_subnet_id
-  ec2_security_group = module.secuiry_group_application.sg_id
-  allow_public_ip    = true
-  ebs_volume_type    = "gp2"
-  ebs_volume_size    = 8
-  key_pair_name      = "moba-key" # enter you key name  
-  depends_on         = [module.secuiry_group_application]
-}
+# module "web_instace_application" {
+#   source             = "./modules/ec2"
+#   instance_name      = "Application-ec2"
+#   instance_type      = "t3.micro"
+#   subnet_id          = module.main_vpc.private_subnet_id
+#   ec2_security_group = module.secuiry_group_application.sg_id
+#   allow_public_ip    = true
+#   ebs_volume_type    = "gp2"
+#   ebs_volume_size    = 8
+#   key_pair_name      = "moba-key" # enter you key name  
+#   depends_on         = [module.secuiry_group_application]
+# }
